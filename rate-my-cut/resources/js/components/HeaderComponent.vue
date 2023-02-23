339c7b2a9f4beae07aefed2a4f64488c50adeffe
KevinGitSar <kevinsar.97@gmail.com>
2023-02-22 19:30:41 -0500

    Creating Main Page

---
+++ rate-my-cut/resources/js/components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container header">
+        <div class="row justify-content-center">
+            <h1>{{ title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['title'],
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<style scoped>
+    .header{
+        display: flex;
+        justify-content: center;
+        align-content: center;
+        flex-direction: column;
+        height: 15vh;
+        background-color: #FFCB77;
+    }
+
+    h1{
+        text-align: center;
+        margin: auto;
+        font-style: italic;
+        font-family: 'K2D', sans-serif;
+        font-size: 8vh;
+        color: #FEF9EF;
+        text-shadow: -2px -2px 0 #000, 2px -2px #000, -2px 2px #000, 2px 2px #000;
+    }
+
+</style>
