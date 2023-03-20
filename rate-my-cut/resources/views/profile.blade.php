b3aba6b70dd2c49ef5baf2009694bf351a39f1ea
KevinGitSar <kevinsar.97@gmail.com>
2023-03-20 14:24:34 -0400

    Continuing profile layout -> working on filter section

--- rate-my-cut/resources/views/profile.blade.php
+++ rate-my-cut/resources/views/profile.blade.php
@@ -22,23 +22,62 @@
         <div id="app">
             <Headercomponent class="max-w-full" title='Rate My Cut!'></Headercomponent>
             <Navbar1component class="max-w-full"></Navbar1component>
-            <div class="flex grow max-w-full mt-5 mb-5">
+            <div class="flex grow h-screen mt-5 mb-5">
                     <!--Profile Info/Bio etc...-->
-                    <div class="w-1/5">
-                        <div class="border-2 border-[#291F1F] h-96 mx-20">
+                    <div class="w-1/5 h-auto">
+                        <div class="border-2 border-[#291F1F] h-full mx-10">
                             <!--Profile Image-->
-                            <img src="https://www.w3schools.com/w3images/avatar2.png" alt="Avatar" class="w-52 h-52 mx-auto rounded-full mt-5">
+                            <img src="https://www.w3schools.com/w3images/avatar2.png" alt="Avatar" class="w-3/5 h-auto mx-auto rounded-full mt-5 mb-5">
+                            
+                            <div class="flex flex-col justify-around">
+                                <p class="my-2 ml-1">User's Name</p>
+
+                                <p class="my-2 ml-1">1000 Followers</p>
+
+                                <p class="my-2 ml-1">Following 100</p>
+
+                                <p class="my-2 ml-1">
+                                    Generally like my hair short,
+                                    prefer asian ethnic hairstyles.
+                                </p>
+
+                            </div>
                         </div>
                     </div>
                     
                     <!--User's uploaded content (images + filtering)-->
-                    <div class="w-3/5 border-2 border-[#291F1F]">
+                    <div class="w-3/5 h-auto border-2 border-[#291F1F]">
+                        <div class="h-full">
+                            <div class="mb-5">
+                                <p>Filter PlaceHolder</p>
+                            </div>
+                            <div class="flex flex-auto justify-between h-5/6">
+
+                                <div class="self-center"><p>Prev</p></div>
+
+                                <div class="flex flex-auto flex-wrap justify-items-start w-max">
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                    <div class="w-1/4 h-1/2 p-2 border-2 border-[#291F1F]"></div>
+                                </div>
+
+                                <div class="self-center"><p>Next</div>
+
+                            </div>
+                        </div>
                     </div>
 
                     <!--Show following-->
-                    <div class="w-1/5">
-                        <div class="border-2 border-[#291F1F] h-96 mx-20">
+                    <div class="w-1/5 h-auto">
+                        <div class="border-2 border-[#291F1F] h-full mx-10">
+                            <h3 class="text-center mt-5 underline decoration-1">Following List</h3>
 
+                            <!--Add Followers Here-->
                         </div>
                     </div>
                 
