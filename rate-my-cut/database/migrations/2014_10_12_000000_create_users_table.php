f03b654741ea90532d4e4ae73488e9695327e141
KevinGitSar <kevinsar.97@gmail.com>
2023-03-29 00:12:45 -0400

    Updated user migration table

--- rate-my-cut/database/migrations/2014_10_12_000000_create_users_table.php
+++ rate-my-cut/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthdate');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->string('postal_code');
+            $table->string('username');
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
